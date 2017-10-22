707196966592704d98439f0018af3e308b13d57c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2017-10-22 21:00:30 +0700

    setup project

---
+++ src/main/java/com/idspring/crudreactive/CrudReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.idspring.crudreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudReactiveApplication.class, args);
+	}
+}
